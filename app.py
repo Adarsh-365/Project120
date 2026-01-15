d98b6b7ff2dd2e62ee794aa01407742f11bf5fda
Adarsh Tayde <102159256+Adarsh-365@users.noreply.github.com>
2026-01-15 20:13:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template ,redirect
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'dev-secret-key')
@@ -8,6 +8,10 @@ app.secret_key = os.environ.get('SECRET_KEY', 'dev-secret-key')
 @app.route("/about")
 def about():
     return render_template("aboutus.html")
+
+@app.route("/memdoc")
+def membersgipdoc():
+    return redirect("https://docs.google.com/document/d/17tn7U1yVHc7fGgMQfd0geisgS_FYTpOG/edit?usp=sharing&ouid=100361699467384810298&rtpof=true&sd=true", code=302)
 
 
 @app.route("/membership")
@@ -26,4 +30,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
